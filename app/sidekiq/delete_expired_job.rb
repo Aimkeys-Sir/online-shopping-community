dd69ccfd61fff073feb3fd4a4c6825a50d08326c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-24 11:36:15 +0300

    add tasks

--- app/sidekiq/delete_expired_job.rb
+++ app/sidekiq/delete_expired_job.rb
@@ -3,7 +3,7 @@ class DeleteExpiredJob
 
   def perform()
     # Do something
-    # expired=Product.where("expire_at <?",Time.now)
-    # expired.destroy_all
+    expired=Product.where("expire_at <?",Time.now)
+    expired.destroy_all
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Product, type: :model do
     # 1. Should be created successifully with valid data.
     # 2. name must exist.
     # 3. shop must exist.[enumerable of existing shops]
-    # 4. 
+    # 4. Testing performace(time consumed during the request)
+    
+
 end
